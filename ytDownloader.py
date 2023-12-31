8b4ecbee1b3c2c9312d1b55f25614cead9127a98
Manoj <penugurthimanoj@gmail.com>
2023-12-31 12:48:51 +0530

    1) You can now download only audio. 2) Minor UI changes.

--- ytDownloader.py
+++ ytDownloader.py
@@ -2,6 +2,7 @@ from pytube import YouTube
 import tkinter as tk
 import customtkinter
 from tkinter import filedialog
+import os
 
 def download_video(choice):
   try:
@@ -34,6 +35,29 @@ def download_video(choice):
     title.configure(text="YT-VIDEO DOWNLOADER")
     finishLabel.configure(text="INVALID URL", text_color="red", font=("Montserrat", 12))
 
+def download_audio():
+  try:
+    ytlink = link.get()
+    ytobject = YouTube(ytlink, on_progress_callback=on_prog)
+    audio = ytobject.streams.filter(only_audio=True).first()
+    title.configure(text=ytobject.title, text_color="white")
+
+    save_path = filedialog.askdirectory()
+    if save_path:
+      finishLabel.configure(text="")
+      finishLabel.configure(text=f"Selected Folder: {save_path}", text_color="white")
+      out_file = audio.download(output_path=save_path)
+      base, ext = os.path.splitext(out_file) 
+      new_file = base + '.mp3'
+      os.rename(out_file, new_file) 
+      finishLabel.configure(text="Audio Downloaded Successfully!", text_color="white") 
+      root = tk.Tk()
+      root.withdraw()
+      
+  except Exception:
+    title.configure(text="YT-VIDEO DOWNLOADER")
+    finishLabel.configure(text="INVALID URL", text_color="red", font=("Montserrat", 12))
+
 def on_prog(stream, chunk, bytes_rem):
   total_size = stream.filesize
   bytes_downloaded = total_size-bytes_rem
@@ -55,39 +79,41 @@ app.title("YT Downloader")
 
 # adding ui elements
 title = customtkinter.CTkLabel(app, text="YT-VIDEO DOWNLOADER", font=("Montserrat", 20, "bold"))
-title.pack(padx=10, pady=50)
+title.pack(padx=10, pady=30, anchor=tk.CENTER)
 
 uv = customtkinter.CTkLabel(app, text="Enter URL", font=("Montserrat", 12, "bold"))
-uv.pack(padx=10, pady=0)
+uv.pack(padx=10, pady=0, anchor=tk.CENTER)
 
 # link input
 url_var = tk.StringVar()
 link = customtkinter.CTkEntry(app, width=350, height=40, textvariable=url_var, font=("Montserrat", 13), corner_radius=5)
-link.pack(padx=1, pady=10)
+link.pack(padx=1, pady=10, anchor=tk.CENTER)
 
 
 
 # finish label
 finishLabel = customtkinter.CTkLabel(app, text="", font=("Montserrat", 12))
-finishLabel.pack(padx=1, pady=10)
+finishLabel.pack(padx=1, pady=10, anchor=tk.CENTER)
 
 # prog bar
 progNum = customtkinter.CTkLabel(app, text="0%", font=("Montserrat", 15))
-progNum.pack(padx=1, pady=0)
+progNum.pack(padx=1, pady=0, anchor=tk.CENTER)
 
 progBar = customtkinter.CTkProgressBar(app, width=400, height=2)
 progBar.set(0)
-progBar.pack(padx=10, pady=15)
+progBar.pack(padx=10, pady=15, anchor=tk.CENTER)
 
-# download button
-# res selection
-rs = customtkinter.CTkLabel(app, text="Select Resolution", font=("Montserrat", 12, "bold"))
-rs.pack(padx=1, pady=10)
+#audio download
+ad = customtkinter.CTkButton(app, text="Download Audio", font=("Montserrat", 12), command = download_audio, corner_radius=5, width=150, height=30)
+ad.pack(padx=1, pady=15, anchor=tk.CENTER)
 
+# res selection / download
+rs = customtkinter.CTkLabel(app, text="Download Video", font=("Montserrat", 12, "bold"))
+rs.pack(padx=1, pady=10, anchor=tk.CENTER)
 
 options = ["Download 360p", "Download 720p", "Download 1080p"]
-res_selection = customtkinter.CTkComboBox(master=app, values=options, width=200, height=30, font=("Montserrat", 12), dropdown_font=("Montserrat", 12), corner_radius=5, state="readonly", command = download_video)
-res_selection.pack(padx=10, pady=0)
+res_selection = customtkinter.CTkComboBox(master=app, values=options, width=200, height=30, font=("Montserrat", 12), dropdown_font=("Montserrat", 12), corner_radius=5, state="readonly", command = download_video, justify="center")
+res_selection.pack(padx=10, pady=0, anchor=tk.CENTER)
 
 # run app
 app.mainloop() 
