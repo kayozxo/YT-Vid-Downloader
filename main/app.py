5f05a1a00d49e9ad965d09c9f26bcd1d8f3430e1
Manoj <penugurthimanoj@gmail.com>
2024-06-02 22:19:45 +0530

    made few changes for streamlit deployment

--- main/app.py
+++ main/app.py
@@ -1,7 +1,11 @@
 import streamlit as st
 from pytube import YouTube
-from tkinter import filedialog
-import os
+import os, re, time
+from streamlit_extras.buy_me_a_coffee import button
+
+directory= 'downloads/'
+if not os.path.exists(directory):
+    os.makedirs(directory)
 
 def get_info(url):
     yt = YouTube(url)
@@ -13,37 +17,42 @@ def get_info(url):
 
 st.set_page_config(page_title="YouTube Downloader", page_icon=":zap:")
 st.title(":zap: YouTube Downloader")
-url = st.text_input("**Enter URL**")
+url = st.text_input("**Paste URL here 👇**", placeholder="https://www.youtube.com/")
+
+code = """
+<a href="https://www.buymeacoffee.com/kayozxo" target="_blank"><img src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=☕&slug=kayozxo&button_colour=40DCA5&font_colour=ffffff&font_family=Lato&outline_colour=000000&coffee_colour=FFDD00" /></a>
+
+"""
+
 
 def download_video():
-  try:
-    yt = YouTube(url)
-    streams = yt.streams.filter(progressive=True, file_extension="mp4")
-    highest_res_stream = streams.get_highest_resolution()
-    DS = highest_res_stream
-    save_path = filedialog.askdirectory()
-    if save_path:
-      DS.download(output_path=save_path)
-      st.success("Video is successfully downloaded!")
-
-  except Exception:
-    st.warning("Invalid URL!")
+  with st.spinner("Downloading..."):
+    try:
+      yt = YouTube(url)
+      streams = yt.streams.filter(progressive=True, file_extension="mp4")
+      highest_res_stream = streams.get_highest_resolution()
+      DS = highest_res_stream
+      save_path = directory
+      if save_path:
+        DS.download(output_path=save_path)
+        st.success("Video is successfully downloaded!")
+    except Exception:
+      st.warning("Invalid URL!")
 
 def download_audio():
-  try:
-    yt = YouTube(url)
-    audio = yt.streams.filter(only_audio=True).first()
-    save_path = filedialog.askdirectory()
-    if save_path:
-      out_file = audio.download(output_path=save_path)
-      base, ext = os.path.splitext(out_file)
-      new_file = base + '.mp3'
-      os.rename(out_file, new_file)
-      st.success("Audio is successfully downloaded!")
-
-  except Exception:
-    st.warning("Invalid URL!")
-
+  with st.spinner("Downloading..."):
+    try:
+      yt = YouTube(url)
+      audio = yt.streams.filter(only_audio=True).first()
+      save_path = directory
+      if save_path:
+        out_file = audio.download(output_path=save_path)
+        base, ext = os.path.splitext(out_file)
+        new_file = base + '.mp3'
+        os.rename(out_file, new_file)
+        st.success("Audio is successfully downloaded!")
+    except Exception:
+      st.warning("Invalid URL!")
 
 col1, col2 = st.columns(2)
 col1.button("Download Video :film_frames:", on_click=download_video, use_container_width=True)
@@ -52,3 +61,6 @@ col2.button("Download Audio :musical_note:", on_click=download_audio, use_contai
 if url:
     v_info = get_info(url)
     st.image(v_info["image"], use_column_width=True, caption=f"{v_info['title']} - {v_info['length']} seconds")
+
+with st.sidebar:
+  st.html(code)
