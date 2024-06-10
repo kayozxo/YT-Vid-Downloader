98fc6bf00e69f9087d08e3d44407317c937394a6
Manoj <penugurthimanoj@gmail.com>
2024-06-10 09:39:09 +0530

    reformatted files

--- main/app.py
+++ main/app.py
@@ -5,7 +5,7 @@ import streamlit_shadcn_ui as ui
 from googleapiclient.discovery import build
 from pytube import extract
 
-directory= 'downloads/'
+directory = "downloads/"
 if not os.path.exists(directory):
     os.makedirs(directory)
 
@@ -19,17 +19,18 @@ code = """
 <a href="https://www.buymeacoffee.com/kayozxo" target="_blank"><img src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=☕&slug=kayozxo&button_colour=40DCA5&font_colour=ffffff&font_family=Lato&outline_colour=000000&coffee_colour=FFDD00" /></a>
 
 """
+
+
 def get_info(url):
     yt = YouTube(url)
 
     video_id = extract.video_id(url)
 
-    youtube = build('youtube', 'v3', developerKey='AIzaSyC54QjnV9FiDRyvwu2_JLoJgnmWq4VoLdo')
-
-    video_request = youtube.videos().list(
-        part='snippet,statistics',
-        id=video_id
+    youtube = build(
+        "youtube", "v3", developerKey="AIzaSyC54QjnV9FiDRyvwu2_JLoJgnmWq4VoLdo"
     )
+
+    video_request = youtube.videos().list(part="snippet,statistics", id=video_id)
     video_response = video_request.execute()
 
     print(video_response)
@@ -38,69 +39,76 @@ def get_info(url):
         "image": yt.thumbnail_url,
         "title": yt.title,
         "length": yt.length,
-        "likes": 'N/A',  # default value
-        "views": 'N/A'   # default value
+        "likes": "N/A",  # default value
+        "views": "N/A",  # default value
     }
 
-    if 'items' in video_response and len(video_response['items']) > 0:
-        video_details = video_response['items'][0]['statistics']
-        details["likes"] = video_details.get('likeCount', 'N/A')
-        details["views"] = video_details.get('viewCount', 'N/A')
+    if "items" in video_response and len(video_response["items"]) > 0:
+        video_details = video_response["items"][0]["statistics"]
+        details["likes"] = video_details.get("likeCount", "N/A")
+        details["views"] = video_details.get("viewCount", "N/A")
 
     return details
 
+
 def download_video():
-  with st.spinner("Downloading..."):
-    try:
-      yt = YouTube(url)
-      streams = yt.streams.filter(progressive=True, file_extension="mp4")
-      highest_res_stream = streams.get_highest_resolution()
-      DS = highest_res_stream
-      save_path = directory
-      if save_path:
-        DS.download(output_path=save_path)
-        st.success("Video is successfully downloaded!")
-    except Exception:
-      st.warning("Invalid URL!")
+    with st.spinner("Downloading..."):
+        try:
+            yt = YouTube(url)
+            streams = yt.streams.filter(progressive=True, file_extension="mp4")
+            highest_res_stream = streams.get_highest_resolution()
+            DS = highest_res_stream
+            save_path = directory
+            if save_path:
+                DS.download(output_path=save_path)
+                st.success("Video is successfully downloaded!")
+        except Exception:
+            st.warning("Invalid URL!")
+
 
 def download_audio():
-  with st.spinner("Downloading..."):
-    try:
-      yt = YouTube(url)
-      audio = yt.streams.filter(only_audio=True).first()
-      save_path = directory
-      if save_path:
-        out_file = audio.download(output_path=save_path)
-        base, ext = os.path.splitext(out_file)
-        new_file = base + '.mp3'
-        os.rename(out_file, new_file)
-        st.success("Audio is successfully downloaded!")
-    except Exception:
-      st.warning("Invalid URL!")
+    with st.spinner("Downloading..."):
+        try:
+            yt = YouTube(url)
+            audio = yt.streams.filter(only_audio=True).first()
+            save_path = directory
+            if save_path:
+                out_file = audio.download(output_path=save_path)
+                base, ext = os.path.splitext(out_file)
+                new_file = base + ".mp3"
+                os.rename(out_file, new_file)
+                st.success("Audio is successfully downloaded!")
+        except Exception:
+            st.warning("Invalid URL!")
+
 
 col1, col2 = st.columns(2)
 with col1:
-  vid_btn = ui.button("Download Video", variant="secondary", key="vid_btn", class_name="w-full")
+    vid_btn = ui.button(
+        "Download Video", variant="secondary", key="vid_btn", class_name="w-full"
+    )
 
 with col2:
-  aud_btn = ui.button("Download Audio", variant="secondary", key="aud_btn", class_name="w-full")
+    aud_btn = ui.button(
+        "Download Audio", variant="secondary", key="aud_btn", class_name="w-full"
+    )
 
 if vid_btn:
-  download_video()
+    download_video()
 
 if aud_btn:
-  download_audio()
+    download_audio()
 
 if url:
     v_info = get_info(url)
     st.image(v_info["image"], use_column_width=True, caption=f"{v_info['title']}")
     cols = st.columns(3)
     with cols[0]:
-      ui.metric_card(title="Total Views", content=f"{v_info['views']}")
+        ui.metric_card(title="Total Views", content=f"{v_info['views']}")
     with cols[1]:
-      ui.metric_card(title="Total Likes", content=f"{v_info['likes']}")
+        ui.metric_card(title="Total Likes", content=f"{v_info['likes']}")
     with cols[2]:
-      ui.metric_card(title="Video Length", content=f"{v_info['length']} sec")
+        ui.metric_card(title="Video Length", content=f"{v_info['length']} sec")
 
 with st.sidebar:
-  st.html(code)
+    st.html(code)
--- main/main.py
+++ main/main.py
@@ -5,80 +5,115 @@ from tkinter import filedialog
 from CTkMessagebox import CTkMessagebox
 from CTkToolTip import *
 
+
 def download_video(choice):
-  try:
-    ytlink = link.get()
-    ytobject = YouTube(ytlink, on_progress_callback=on_prog)
-    streams = ytobject.streams.filter(progressive=True, file_extension="mp4")
+    try:
+        ytlink = link.get()
+        ytobject = YouTube(ytlink, on_progress_callback=on_prog)
+        streams = ytobject.streams.filter(progressive=True, file_extension="mp4")
 
-    if choice == options[0]:
-      r360 = streams.filter(res="360p").first()
-      DS = r360
-    elif choice == options[1]:
-      r720 = streams.filter(res="720p").first()
-      DS = r720
-    else:
-      highest_res_stream = streams.get_highest_resolution()
-      DS = highest_res_stream
+        if choice == options[0]:
+            r360 = streams.filter(res="360p").first()
+            DS = r360
+        elif choice == options[1]:
+            r720 = streams.filter(res="720p").first()
+            DS = r720
+        else:
+            highest_res_stream = streams.get_highest_resolution()
+            DS = highest_res_stream
 
-    title.configure(text=ytobject.title, text_color="white")
+        title.configure(text=ytobject.title, text_color="white")
 
-    save_path = filedialog.askdirectory()
-    if save_path:
-      finishLabel.configure(text="")
-      finishLabel.configure(text=f"Selected Folder: {save_path}", text_color="white")
-      DS.download(output_path=save_path)
-      CTkMessagebox(title="Success", message="Video is successfully downloaded.", icon="check")
+        save_path = filedialog.askdirectory()
+        if save_path:
+            finishLabel.configure(text="")
+            finishLabel.configure(
+                text=f"Selected Folder: {save_path}", text_color="white"
+            )
+            DS.download(output_path=save_path)
+            CTkMessagebox(
+                title="Success",
+                message="Video is successfully downloaded.",
+                icon="check",
+            )
 
-  except Exception:
-    title.configure(text="YT-VIDEO DOWNLOADER")
-    CTkMessagebox(title="Invalid URL!", message="Please Enter a Valid URL!", icon="cancel", option_1="Cancel", option_2="Retry")
+    except Exception:
+        title.configure(text="YT-VIDEO DOWNLOADER")
+        CTkMessagebox(
+            title="Invalid URL!",
+            message="Please Enter a Valid URL!",
+            icon="cancel",
+            option_1="Cancel",
+            option_2="Retry",
+        )
+
 
 def download_audio():
-  try:
-    ytlink = link.get()
-    ytobject = YouTube(ytlink, on_progress_callback=on_prog)
-    audio = ytobject.streams.filter(only_audio=True).first()
-    title.configure(text=ytobject.title, text_color="white")
+    try:
+        ytlink = link.get()
+        ytobject = YouTube(ytlink, on_progress_callback=on_prog)
+        audio = ytobject.streams.filter(only_audio=True).first()
+        title.configure(text=ytobject.title, text_color="white")
 
-    save_path = filedialog.askdirectory()
-    if save_path:
-      finishLabel.configure(text="")
-      finishLabel.configure(text=f"Selected Folder: {save_path}", text_color="white")
-      out_file = audio.download(output_path=save_path)
-      base, ext = os.path.splitext(out_file)
-      new_file = base + '.mp3'
-      os.rename(out_file, new_file)
-      CTkMessagebox(title="Succes", message="Audio is successfully downloaded.", icon="check")
+        save_path = filedialog.askdirectory()
+        if save_path:
+            finishLabel.configure(text="")
+            finishLabel.configure(
+                text=f"Selected Folder: {save_path}", text_color="white"
+            )
+            out_file = audio.download(output_path=save_path)
+            base, ext = os.path.splitext(out_file)
+            new_file = base + ".mp3"
+            os.rename(out_file, new_file)
+            CTkMessagebox(
+                title="Succes",
+                message="Audio is successfully downloaded.",
+                icon="check",
+            )
 
+    except Exception:
+        title.configure(text="YT-VIDEO DOWNLOADER")
+        CTkMessagebox(
+            title="Invalid URL!",
+            message="Please Enter a Valid URL!",
+            icon="cancel",
+            option_1="Cancel",
+            option_2="Retry",
+        )
 
-  except Exception:
-    title.configure(text="YT-VIDEO DOWNLOADER")
-    CTkMessagebox(title="Invalid URL!", message="Please Enter a Valid URL!", icon="cancel", option_1="Cancel", option_2="Retry")
 
 def download_thumbnail():
-  try:
-    ytlink = link.get()
-    ytobject = YouTube(ytlink)
-    title.configure(text=ytobject.title, text_color="white")
-    exp = "^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*"
-    s = re.findall(exp,ytlink)[0][-1]
-    thumbnail = f"https://i.ytimg.com/vi/{s}/maxresdefault.jpg"
-    webbrowser.open(thumbnail)
+    try:
+        ytlink = link.get()
+        ytobject = YouTube(ytlink)
+        title.configure(text=ytobject.title, text_color="white")
+        exp = (
+            "^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*"
+        )
+        s = re.findall(exp, ytlink)[0][-1]
+        thumbnail = f"https://i.ytimg.com/vi/{s}/maxresdefault.jpg"
+        webbrowser.open(thumbnail)
 
-  except Exception:
-    title.configure(text="YT-VIDEO DOWNLOADER")
-    CTkMessagebox(title="Invalid URL!", message="Please Enter a Valid URL!", icon="cancel", option_1="Cancel", option_2="Retry")
+    except Exception:
+        title.configure(text="YT-VIDEO DOWNLOADER")
+        CTkMessagebox(
+            title="Invalid URL!",
+            message="Please Enter a Valid URL!",
+            icon="cancel",
+            option_1="Cancel",
+            option_2="Retry",
+        )
 
 
 def on_prog(stream, chunk, bytes_rem):
-  total_size = stream.filesize
-  bytes_downloaded = total_size-bytes_rem
-  percentage_of_completion = bytes_downloaded / total_size * 100
-  per = str(int(percentage_of_completion))
-  progNum.configure(text=per + "%")
-  progNum.update()
-  progBar.set(float(percentage_of_completion / 100))
+    total_size = stream.filesize
+    bytes_downloaded = total_size - bytes_rem
+    percentage_of_completion = bytes_downloaded / total_size * 100
+    per = str(int(percentage_of_completion))
+    progNum.configure(text=per + "%")
+    progNum.update()
+    progBar.set(float(percentage_of_completion / 100))
+
 
 # system settings
 customtkinter.set_appearance_mode("System")
@@ -92,7 +127,9 @@ app.title("YT Downloader")
 app.resizable(False, False)
 
 # adding ui elements
-title = customtkinter.CTkLabel(app, text="YT-VIDEO DOWNLOADER", font=("Montserrat Black", 20))
+title = customtkinter.CTkLabel(
+    app, text="YT-VIDEO DOWNLOADER", font=("Montserrat Black", 20)
+)
 title.place(x=400, y=50, anchor="center")
 
 uv = customtkinter.CTkLabel(app, text="Enter URL", font=("Montserrat", 12, "bold"))
@@ -100,7 +137,14 @@ uv.place(x=180, y=95)
 
 # link input
 url_var = tk.StringVar()
-link = customtkinter.CTkEntry(app, width=450, height=40, textvariable=url_var, font=("Montserrat", 13), corner_radius=5)
+link = customtkinter.CTkEntry(
+    app,
+    width=450,
+    height=40,
+    textvariable=url_var,
+    font=("Montserrat", 13),
+    corner_radius=5,
+)
 link.place(x=400, y=140, anchor="center")
 
 CTkToolTip(link, message="Paste a Valid YouTube Video URL!")
@@ -115,19 +159,46 @@ progBar = customtkinter.CTkProgressBar(app, width=800, height=5)
 progBar.set(0)
 progBar.place(x=0, y=0)
 
-#audio download
-ad = customtkinter.CTkButton(app, text="Download Audio", font=("Montserrat Medium", 12), command = download_audio, corner_radius=5, width=140, height=34)
+# audio download
+ad = customtkinter.CTkButton(
+    app,
+    text="Download Audio",
+    font=("Montserrat Medium", 12),
+    command=download_audio,
+    corner_radius=5,
+    width=140,
+    height=34,
+)
 ad.place(x=175, y=170)
 
 CTkToolTip(ad, message="Download Audio at Highest Quality!")
 # res selection / download
 options = ["Download 360p", "Download 720p", "Download 1080p"]
-res_selection = customtkinter.CTkComboBox(master=app, values=options, width=140, height=34, font=("Montserrat Medium", 12), dropdown_font=("Montserrat Medium", 12), corner_radius=5, state="readonly", command = download_video, justify="center")
+res_selection = customtkinter.CTkComboBox(
+    master=app,
+    values=options,
+    width=140,
+    height=34,
+    font=("Montserrat Medium", 12),
+    dropdown_font=("Montserrat Medium", 12),
+    corner_radius=5,
+    state="readonly",
+    command=download_video,
+    justify="center",
+)
 res_selection.place(x=330, y=170)
 
 CTkToolTip(res_selection, message="Download Video at Multiple Resolutions!")
 # thumbnail download
-td = customtkinter.CTkButton(app, text="Download Image", font=("Montserrat Medium", 12), command = download_thumbnail, corner_radius=5, width=140, height=34)
+td = customtkinter.CTkButton(
+    app,
+    text="Download Image",
+    font=("Montserrat Medium", 12),
+    command=download_thumbnail,
+    corner_radius=5,
+    width=140,
+    height=34,
+)
 td.place(x=485, y=170)
 
 CTkToolTip(td, message="Download Thumbnail at Highest Quality!")
